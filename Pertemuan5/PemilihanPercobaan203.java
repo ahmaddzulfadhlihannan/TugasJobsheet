1711cf7fb1d1679b7c799e462a1f11e42d551057
ahmaddzulfadhlihannan <ahmadriski207@gmail.com>
2023-09-27 09:44:43 +0700

    Komitan anyar

--- Pertemuan5/PemilihanPercobaan203.java
+++ Pertemuan5/PemilihanPercobaan203.java
@@ -19,12 +19,37 @@ public class PemilihanPercobaan203 {
         //Operasi
         float total = (uas * 0.4f) + (uts * 0.3f) + (kuis * 0.1f) + (tugas * 0.2f);
 
-        //Remidi
-        String messege = total < 65 ? "Remidi" : "Tidak Remidi";
+        //Remidi    
+        String messege = total < 65 ? "Remidi" : "Tidak remidi";
 
         //Hasil
-        System.out.print("Nilai akhir = " + total + "sehingga " + messege);
+        System.out.println("Nilai akhir = " + total + " sehingga " + messege);
 
+        //Nilai huruf
+        if (total > 80 && total <= 100) {
+            System.out.print("Nilai huruf anda A\n");
+        }
+        else if (total > 73 && total <= 80) {
+            System.out.print("Nilai huruf anda B+\n");
+        }
+        else if (total > 65 && total <= 73) {
+            System.out.print("Nilai huruf anda B\n");
+        }
+        else if (total > 60 && total <= 65) {
+            System.out.print("Nilai huruf anda C+\n");
+        }
+        else if (total > 50 && total <= 60) {
+            System.out.print("Nilai huruf anda C\n");
+        }
+        else if (total > 39 && total <= 50) {
+            System.out.print("Nilai huruf anda D\n");
+        }
+        else if (total <= 39) {
+            System.out.print("Nilai huruf anda E\n");
+        }
+        else {
+            System.out.print("Tidak valid\n");
+        }
         i.close();
     }
 }
