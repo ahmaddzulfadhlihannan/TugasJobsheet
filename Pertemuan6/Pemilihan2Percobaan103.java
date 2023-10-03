0bd26e7503d4c640a697c2a1a1deac3bb45c390b
Hannan <ahmadriski207@gmail.com>
2023-10-04 00:54:34 +0700

    komit p2 ke 1

---
+++ Pertemuan6/Pemilihan2Percobaan103.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan103 {
+    public static void main(String[] args) {
+
+        //Scanner
+        Scanner input03 = new Scanner(System.in);
+
+        //Varibel
+        int tahun;
+
+        //Masukkan input
+        System.out.print("Masukkan tahun\t\t:\t\t");
+        tahun = input03.nextInt();
+
+        //Pemilihan bersarang
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0) {
+                System.out.print("Tahun Kabisat");
+            } 
+        }
+        else {
+            System.out.print("Bukan tahun kabisat");
+        }
+
+        //Tutup Scanner
+        input03.close();
+
+        //entah apa yang merasuki mueh
+
+    }
+}
