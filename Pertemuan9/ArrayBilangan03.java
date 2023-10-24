15e1625eed66e55506b6b91584db0264077f71ee
Hannan <ahmadriski207@gmail.com>
2023-10-24 16:40:38 +0700

    P9 ke 2

--- Pertemuan9/ArrayBilangan03.java
+++ Pertemuan9/ArrayBilangan03.java
@@ -1,20 +1,14 @@
 public class ArrayBilangan03 {
     public static void main(String[] args) {
         
-        //Variabel
-        int[] bil = new int [4];
-
-        //Nilai indeks
-        bil[0] = 5;
-        bil[2] = 13;
-        bil[2] = -7;
-        bil[3] = -17;
+        //Variabel setelah modifikasi
+        int bil[] = {5, 13, -7, -17} ;
 
         //Tampilkan nilai indeks
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        int i;
+        for (i = 0; i <= 4; i++) {
+            System.out.println(bil[i]);
+        }
 
     }
 }
