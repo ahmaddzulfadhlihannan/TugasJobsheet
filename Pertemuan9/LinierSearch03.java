1862d2deef582e95b23dc8115b77e59e7401c0cb
ahmaddzulfadhlihannan <ahmadriski207@gmail.com>
2023-10-25 08:18:51 +0700

    P9 ke 12

--- Pertemuan9/LinierSearch03.java
+++ Pertemuan9/LinierSearch03.java
@@ -1,15 +1,40 @@
+import java.util.Scanner;
 public class LinierSearch03 {
     public static void main(String[] args) {
-        int[] arrrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
+        Scanner sc = new Scanner(System.in);
+
+        //Input jumlah elemen
+        System.out.print("Masukkan jumlah elemen : ");
+        int jumlahElemen = sc.nextInt();
+        
+        //Variabel
+        int[] arrayInt = new int[jumlahElemen];
+        int key;
         int hasil = 0;
 
-        for (int i = 0; i < arrrayInt.length; i++) {
-            if (arrrayInt[i] == key) {
+        //Input elemen
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen array ke-" + i + " : ");
+            arrayInt[i] = sc.nextInt();
+        }
+
+        //Key yag ingin dicari
+        System.out.print("Masukkan elemen yang ingin dicari : ");
+        key = sc.nextInt();
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] == key) {
                 hasil = i;
+                System.out.println("Key ada dalam array posisi ke-" + hasil);
+                break;
+            }
+            else {
+                System.out.println("Key tidak ditemukan");
                 break;
             }
         }
-        System.out.println("Key ada dalam array posisi ke-" + hasil);
+        
+
+        sc.close();
     }
 }
