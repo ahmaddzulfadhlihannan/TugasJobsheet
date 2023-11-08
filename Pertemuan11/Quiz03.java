2df167496cf7e80813a59fda552d2f81c6bc6b3d
Hannan <ahmadriski207@gmail.com>
2023-11-08 09:12:55 +0700

    P11 ke 3

---
+++ Pertemuan11/Quiz03.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Quiz03 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+        char menu = 'y';
+
+        do {
+            int number = random.nextInt(10) + 1;
+            boolean success = false;
+            do {
+                System.out.print("Tebak angka (1-10): ");
+                int answer = input.nextInt();
+                input.nextLine();
+                if (answer < number) {
+                    System.out.println("Tebakan terlalu kecil!");
+                } else if (answer > number) {
+                    System.out.println("Tebakan terlalu besar!");
+                } else {
+                    success = true;
+                }
+            } while (!success);
+            System.out.print("Apakah Anda ingin mengulang permainan (Y/y)? ");
+            menu = input.nextLine().charAt(0);
+        } while (menu == 'y' || menu == 'Y');
+        if (menu != 'y' && menu != 'Y') {
+            System.out.println("Permainan dihentikan. Terima kasih telah bermain!");
+        }
+        input.close();
+    }
+}
