f87784e5de3ff1b4c6b282f82764eaee57a86968
ahmaddzulfadhlihannan <ahmadriski207@gmail.com>
2023-11-29 08:24:02 +0700

    P14 ke 3

--- Pertemuan14/Rekursif/Tugas2.java
+++ Pertemuan14/Rekursif/Tugas2.java
@@ -11,12 +11,27 @@ public class Tugas2 {
             return (hasil);
         }
     }
+    static int penjumlahanIteratif(int f) {
+        int hasil = 0;
+        for (int i = 1; i <= f; i++) {
+            hasil += i;
+            if (hasil == 1) {
+                System.out.print("1 ");
+            }
+            else {
+                System.out.print("+ " + i + " ");
+            }
+        }
+        return hasil;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan nilai f : ");
         int f = sc.nextInt();
-        int hasil = penjumlahanRekursif(f);
-        System.out.println(" = " +  + hasil);
+        int hasilRekursif = penjumlahanRekursif(f);
+        System.out.println(" = " +  hasilRekursif);
+        int hasilIteratif = penjumlahanIteratif(f);
+        System.out.println(" = " + hasilIteratif);
         sc.close();
     }
 
